13454121cfe65da5e5535dc43be52e85cbc92d22
Maxim <m_skorokhod@kbtu.kz>
2023-04-11 22:47:58 +0600

    Audio update

--- Lab8,9/Exercise 2/Exercise 2.py
+++ Lab8,9/Exercise 2/Exercise 2.py
@@ -3,6 +3,7 @@ from random import randrange
 import random
 from datetime import timedelta, datetime
 import time
+from pygame import mixer
 
 # Sizes
 resolution = 800
@@ -25,6 +26,7 @@ clock = pygame.time.Clock()
 
 # Initializing fonts, images
 pygame.init()
+mixer.init()
 surface = pygame.display.set_mode([resolution, resolution])
 clock = pygame.time.Clock()
 font_score = pygame.font.SysFont('Calibri', 26, bold=True)
@@ -35,6 +37,11 @@ field = pygame.image.load('Lab8,9\Exercise 2\Images\\field.png').convert_alpha()
 pygame.display.set_caption('Snake')
 icon = pygame.image.load('Lab8,9\Exercise 2\Images\icon.png')
 pygame.display.set_icon(icon)
+
+# Load a background music
+mixer.music.load("Lab8,9\Exercise 2\Audio\music.mp3")
+mixer.music.set_volume(1)
+mixer.music.play(-1)
 
 # Main loop
 RUN = True
@@ -64,6 +71,7 @@ while RUN:
     # Eating apple
     if snake[-1] == apple:
         apple = randrange(0, resolution, size), randrange(0, resolution, size)
+        pygame.mixer.Sound("Lab8,9\Exercise 2\Audio\eating.mp3").play()
         length += 1
         score += 1
         FPS += 1
@@ -71,6 +79,7 @@ while RUN:
         temp = random.randint(0, 2)
     elif snake[-1] == blueberry:
         blueberry = randrange(0, resolution, size), randrange(0, resolution, size)
+        pygame.mixer.Sound("Lab8,9\Exercise 2\Audio\eating.mp3").play()
         length += 3
         score += 3
         FPS += 1
@@ -78,6 +87,7 @@ while RUN:
         temp = random.randint(0, 2)
     elif snake[-1] == goldenapple:
         goldenapple = randrange(0, resolution, size), randrange(0, resolution, size)
+        pygame.mixer.Sound("Lab8,9\Exercise 2\Audio\eating.mp3").play()
         length += 5
         score += 5
         FPS += 1
@@ -86,6 +96,8 @@ while RUN:
         
     # Game over 
     if x < 0 or x > resolution - size or y < 0 or y > resolution - size or len(snake) != len(set(snake)):
+        pygame.mixer.Sound("Lab8,9\Exercise 2\Audio\death.mp3").play()
+        pygame.mixer.music.stop()
         while True:
             render_end = font_end.render('GAME OVER', 1, pygame.Color('red'))
             surface.blit(render_end, (resolution // 2 - 200, resolution // 3))
@@ -127,5 +139,6 @@ while RUN:
         apple = randrange(0, resolution, size), randrange(0, resolution, size)
         blueberry = randrange(0, resolution, size), randrange(0, resolution, size)
         goldenapple = randrange(0, resolution, size), randrange(0, resolution, size)
+        pygame.mixer.Sound("Lab8,9\Exercise 2\Audio\\teleport.mp3").play()
         time_now = datetime.now()
     
